779b25c7abde7050ab41d9a1e211a869c9624805
castro <antoniog_castro@hotmail.com>
2025-07-02 18:34:04 -0300

    feat: exportação e geração multipla de senhas #4 #8

--- src/script.js
+++ src/script.js
@@ -16,15 +16,30 @@ function generatePassword() {
   if (useNumbers) charPool += numberChars;
   if (useSymbols) charPool += symbolChars;
 
-  let password = "";
+  let generatedPasswords = [];
+  let displayText = "";
+
   if (charPool.length === 0) {
-    password = "Selecione pelo menos uma opção!";
+    displayText = "Selecione pelo menos uma opção!";
   } else {
-    for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * charPool.length);
-      password += charPool[randomIndex];
+    for (let j = 0; j < 3; j++) {
+      let password = "";
+      for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charPool.length);
+        password += charPool[randomIndex];
+      }
+      generatedPasswords.push(password);
+      displayText += `Senha ${j + 1}: ${password}\n`;
     }
+
+    const blob = new Blob([generatedPasswords.join("\n")], { type: "text/plain" });
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = "senhas.txt";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   }
 
-  document.getElementById("result").innerText = password;
+  document.getElementById("result").innerText = displayText;
 }
